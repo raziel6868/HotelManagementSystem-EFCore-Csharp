cb3518821aa16a9976ad1acd4a72c200fd9e4bfc
raziel6868 <minhluonganh@gmail.com>
2024-11-10 16:12:42 +0700

    Models update

---
+++ DataAccessLayer/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer.Models;
+
+public partial class Customer
+{
+    public int CustomerId { get; set; }
+
+    public string? CustomerFullName { get; set; }
+
+    public string? Telephone { get; set; }
+
+    public string? EmailAddress { get; set; }
+
+    public DateOnly? CustomerBirthday { get; set; }
+
+    public string? CustomerStatus { get; set; }
+
+    public string? Password { get; set; }
+
+    public virtual ICollection<BookingReservation> BookingReservations { get; set; } = new List<BookingReservation>();
+}
---
+++ DataAccessLayer/Models/FuminiHotelManagementContext.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Models;
+
+public partial class FuminiHotelManagementContext : DbContext
+{
+    public FuminiHotelManagementContext()
+    {
+    }
+
+    public FuminiHotelManagementContext(DbContextOptions<FuminiHotelManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<BookingDetail> BookingDetails { get; set; }
+
+    public virtual DbSet<BookingReservation> BookingReservations { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<RoomInformation> RoomInformations { get; set; }
+
+    public virtual DbSet<RoomType> RoomTypes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server =(local); database = FUMiniHotelManagement;uid=sa;pwd=123;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<BookingDetail>(entity =>
+        {
+            entity.HasKey(e => new { e.BookingReservationId, e.RoomId }).HasName("PK__BookingD__219A5D2EC14D6D89");
+
+            entity.ToTable("BookingDetail");
+
+            entity.Property(e => e.BookingReservationId).HasColumnName("BookingReservationID");
+            entity.Property(e => e.RoomId).HasColumnName("RoomID");
+            entity.Property(e => e.ActualPrice).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.BookingReservation).WithMany(p => p.BookingDetails)
+                .HasForeignKey(d => d.BookingReservationId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BookingDe__Booki__412EB0B6");
+
+            entity.HasOne(d => d.Room).WithMany(p => p.BookingDetails)
+                .HasForeignKey(d => d.RoomId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__BookingDe__RoomI__4222D4EF");
+        });
+
+        modelBuilder.Entity<BookingReservation>(entity =>
+        {
+            entity.HasKey(e => e.BookingReservationId).HasName("PK__BookingR__A2B23EBFF97282B4");
+
+            entity.ToTable("BookingReservation");
+
+            entity.Property(e => e.BookingReservationId)
+                .ValueGeneratedNever()
+                .HasColumnName("BookingReservationID");
+            entity.Property(e => e.BookingStatus).HasMaxLength(20);
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.TotalPrice).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.BookingReservations)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK__BookingRe__Custo__3E52440B");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64B85A3904A8");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.CustomerId)
+                .ValueGeneratedNever()
+                .HasColumnName("CustomerID");
+            entity.Property(e => e.CustomerFullName).HasMaxLength(100);
+            entity.Property(e => e.CustomerStatus).HasMaxLength(20);
+            entity.Property(e => e.EmailAddress).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(100);
+            entity.Property(e => e.Telephone).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<RoomInformation>(entity =>
+        {
+            entity.HasKey(e => e.RoomId).HasName("PK__RoomInfo__32863919755E60E1");
+
+            entity.ToTable("RoomInformation");
+
+            entity.Property(e => e.RoomId)
+                .ValueGeneratedNever()
+                .HasColumnName("RoomID");
+            entity.Property(e => e.RoomNumber).HasMaxLength(10);
+            entity.Property(e => e.RoomPricePerDay).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.RoomStatus).HasMaxLength(20);
+            entity.Property(e => e.RoomTypeId).HasColumnName("RoomTypeID");
+
+            entity.HasOne(d => d.RoomType).WithMany(p => p.RoomInformations)
+                .HasForeignKey(d => d.RoomTypeId)
+                .HasConstraintName("FK__RoomInfor__RoomT__398D8EEE");
+        });
+
+        modelBuilder.Entity<RoomType>(entity =>
+        {
+            entity.HasKey(e => e.RoomTypeId).HasName("PK__RoomType__BCC89611A2919517");
+
+            entity.ToTable("RoomType");
+
+            entity.Property(e => e.RoomTypeId)
+                .ValueGeneratedNever()
+                .HasColumnName("RoomTypeID");
+            entity.Property(e => e.RoomTypeName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
